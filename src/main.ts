71ea678fa2613d00945c6a48ad820d13716e948c
samarmeena <sumarmeena@gmail.com>
2024-06-06 22:15:44 +0530

    refactor: use lavalink player

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { dirname, importx } from "@discordx/importer";
-import { YTDLPlayerPlugin } from "@discordx/plugin-ytdl-player";
+import { LavaPlayerPlugin } from "@discordx/plugin-lava-player";
 import type { Interaction, Message } from "discord.js";
 import { IntentsBitField, Partials } from "discord.js";
 import { Client, MetadataStorage } from "discordx";
 
 import env from "./env.js";
 
-const ytdlPlayerPlugin = new YTDLPlayerPlugin({
+const lavaPlayer = new LavaPlayerPlugin({
   metadata: MetadataStorage.instance,
 });
 
@@ -32,7 +32,7 @@ export const bot = new Client({
   ],
 
   // plugins
-  plugins: [ytdlPlayerPlugin],
+  plugins: [lavaPlayer],
 
   // Debug logs are disabled in silent mode
   silent: false,
@@ -43,35 +43,24 @@ export const bot = new Client({
   },
 });
 
-bot.once("ready", async () => {
-  // Make sure all guilds are cached
-  await bot.guilds.fetch();
-
+bot.once("ready", () => {
   // Synchronize applications commands with Discord
-  await bot.initApplicationCommands();
-
-  // To clear all guild commands, uncomment this line,
-  // This is useful when moving from guild commands to global commands
-  // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
-
-  console.log("Bot started");
+  void bot.initApplicationCommands();
 
   // set bot activity
   if (bot.user) {
     bot.user.setActivity("discordx.js.org");
   }
+
+  console.log("Bot started");
 });
 
 bot.on("interactionCreate", (interaction: Interaction) => {
   bot.executeInteraction(interaction);
 });
 
-bot.on("messageCreate", async (message: Message) => {
-  await bot.executeCommand(message);
+bot.on("messageCreate", (message: Message) => {
+  void bot.executeCommand(message);
 });
 
 async function run() {
